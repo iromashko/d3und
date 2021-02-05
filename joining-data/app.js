9f413b9b6acd97d8785b1eb059e5806bf5826a28
iromashko <iromashko@me.com>
2021-02-05 08:21:12 +0300

    part 3

--- joining-data/app.js
+++ joining-data/app.js
@@ -4,7 +4,19 @@ const el = d3
   .select('ul')
   .selectAll('li')
   .data(data)
-  .join('li')
-  .text(d => d);
+  // .join(
+  //   (enter) => {
+  //     return enter.append('li').style('color', 'purple');
+  //   },
+  //   (update) => update.style('color', 'green'),
+  //   (exit) => exit.remove()
+  // )
+  .text((d) => d);
+
+el.enter()
+  .append('li')
+  .text((d) => d);
+
+el.exit().remove();
 
 console.log(el);
