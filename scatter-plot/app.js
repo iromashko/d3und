efbdcd45fd014c137edcce35de31b02aefb962ae
iromashko <iromashko@me.com>
2021-02-05 09:20:26 +0300

    part 3

---
+++ scatter-plot/app.js
@@ -0,0 +1,17 @@
+async function draw() {
+  const dataset = await d3.json('data.json');
+
+  let dimensions = {
+    width: 800,
+    height: 800,
+  };
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+}
+
+console.log(`Hello`)
+draw();
