2eaf977c608826e3d2c58565decee48041fc5cfc
iromashko <iromashko@me.com>
2021-02-07 00:27:37 +0300

    part 12

--- scatter-plot/app.js
+++ scatter-plot/app.js
@@ -33,6 +33,8 @@ async function draw() {
       `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
     );
 
+  const tooltip = d3.select('#tooltip');
+
   const xScale = d3
     .scaleLinear()
     .domain(d3.extent(dataset, xAccessor))
@@ -54,7 +56,31 @@ async function draw() {
     .attr('cy', (d) => yScale(yAccessor(d)))
     .attr('r', 5)
     .attr('fill', 'red')
-    .attr('data-temp', yAccessor);
+    .attr('data-temp', yAccessor)
+    .on('mouseenter', function (e, datum) {
+      d3.select(this).attr('fill', '#120078').attr('r', 8);
+
+      tooltip
+        .style('display', 'block')
+        .style('top', yScale(yAccessor(datum)) - 25 + 'px')
+        .style('left', xScale(xAccessor(datum)) + 'px');
+
+      const formatter = d3.format('.2f');
+      const dateFormatter = d3.timeFormat('%B %-d, %Y');
+
+      tooltip
+        .select('.metric-humidity span')
+        .text(formatter(xAccessor(datum)));
+      tooltip.select('.metric-temp span').text(formatter(yAccessor(datum)));
+      tooltip
+        .select('.metric-date')
+        .text(dateFormatter(datum.currently.time * 1000));
+    })
+    .on('mouseleave', function (e, datum) {
+      d3.select(this).attr('fill', 'red').attr('r', 5);
+
+      tooltip.style('display', 'none');
+    });
 
   const xAxis = d3
     .axisBottom(xScale)
@@ -62,7 +88,7 @@ async function draw() {
     .tickFormat((d) => d * 100 + '%');
 
   //tickValues([0.3,0.7,0.9])
-  
+
   const yAxis = d3.axisLeft(yScale);
 
   const yAxisGroup = ctr.append('g').call(yAxis).classed('axis', true);
