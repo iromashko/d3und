2fca988f19683e527873b7c74be1bde5bbc6adc3
iromashko <iromashko@me.com>
2021-02-06 23:25:15 +0300

    part 11

--- histogram/app.js
+++ histogram/app.js
@@ -32,6 +32,8 @@ async function draw() {
     .append('g')
     .style('transform', `translateY(${dimensions.ctrHeight}px)`);
 
+  const meanLine = ctr.append('line').classed('mean-line', true);
+
   function histogram(metric) {
     const xAccessor = (d) => d.currently[metric];
     const yAccessor = (d) => d.length;
@@ -51,6 +53,9 @@ async function draw() {
     const newDataset = bin(dataset);
     const padding = 1;
 
+    const exitTransition = d3.transition().duration(500);
+    const updateTransition = exitTransition.transition().duration(500);
+
     const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(newDataset, yAccessor)])
@@ -60,7 +65,24 @@ async function draw() {
     labelsGroup
       .selectAll('text')
       .data(newDataset)
-      .join('text')
+      .join(
+        (enter) =>
+          enter
+            .append('text')
+            .attr(
+              'x',
+              (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2
+            )
+            .attr('y', dimensions.ctrHeight)
+            .text(yAccessor),
+        (update) => update,
+        (exit) =>
+          exit
+            .transition(exitTransition)
+            .attr('y', dimensions.ctrHeight)
+            .remove()
+      )
+      .transition(updateTransition)
       .attr('x', (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
       .attr('y', (d) => yScale(yAccessor(d)) - 10)
       .text(yAccessor);
@@ -68,7 +90,27 @@ async function draw() {
     ctr
       .selectAll('rect')
       .data(newDataset)
-      .join('rect')
+      .join(
+        (enter) =>
+          enter
+            .append('rect')
+            .attr('width', (d) =>
+              d3.max([0, xScale(d.x1) - xScale(d.x0) - padding])
+            )
+            .attr('height', 0)
+            .attr('x', (d) => xScale(d.x0))
+            .attr('y', dimensions.ctrHeight)
+            .attr('fill', '#b8de6f'),
+        (update) => update,
+        (exit) =>
+          exit
+            .attr('fill', '#f39233')
+            .transition(exitTransition)
+            .attr('y', dimensions.ctrHeight)
+            .attr('height', 0)
+            .remove()
+      )
+      .transition(updateTransition)
       .attr('width', (d) =>
         d3.max([0, xScale(d.x1) - xScale(d.x0) - padding])
       )
@@ -77,9 +119,19 @@ async function draw() {
       .attr('y', (d) => yScale(yAccessor(d)))
       .attr('fill', '#01c5c4');
 
+    const mean = d3.mean(dataset, xAccessor);
+
+    meanLine
+      .raise()
+      .transition(updateTransition)
+      .attr('x1', xScale(mean))
+      .attr('y1', 0)
+      .attr('x2', xScale(mean))
+      .attr('y2', dimensions.ctrHeight);
+
     const xAxis = d3.axisBottom(xScale);
 
-    xAxisGroup.call(xAxis);
+    xAxisGroup.transition().call(xAxis);
   }
 
   d3.select('#metric').on('change', function (e) {
